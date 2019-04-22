f11512dc06ea265c90d185e294c7ffe063637fa1
Evan Longwood <evanmlongwood@gmail.com>
2019-04-22 16:08:16 -0700

    Made a table template for the To-do list.

---
+++ react-tutorial/src/components/App.js
@@ -0,0 +1,16 @@
+import React, {Component } from 'react';
+import Table from './Table';
+
+
+
+class App extends Component {
+    render() {
+        return (
+            <div className="container">
+                <Table />
+            </div>
+        )
+    }
+}
+
+export default App
--- react-tutorial/src/index.js
+++ react-tutorial/src/index.js
@@ -1,16 +1,8 @@
-import React, {Component } from 'react';
+import React from 'react';
 import ReactDOM from 'react-dom';
-
+import App from './components/App';
 import './index.css';
 
-class App extends Component {
-    render() {
-        return (
-            <div className="App">
-                <h1>Hello, React!</h1>
-            </div>
-        )
-    }
-}
+
 
 ReactDOM.render(<App/>, document.getElementById('root'));
